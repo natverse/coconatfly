8df6b80f0786684b43139915883933c819c3a1ba
Gregory Jefferis <jefferis@gmail.com>
2023-05-05 16:46:10 +0100

    triple_cosine_plot now has keys as labels

--- tests/testthat/test-triple.R
+++ tests/testthat/test-triple.R
@@ -6,9 +6,9 @@ test_that("triple_cosine_plot works", {
     hc <- triple_cosine_plot('AOTU063', partners = 'o', heatmap = F),
             'hclust')
 
-  bl=list(labels = c("800929667", "791039731", "720575940620326253",
-                     "720575940621925631", "720575940631129362",
-                     "720575940618697118"),
+  bl=list(labels = c("hb:800929667", "hb:791039731", "fw:720575940620326253",
+                     "fw:720575940621925631", "fw:720575940631129362",
+                     "fw:720575940618697118"),
           order = c(1L, 3L, 5L, 2L, 4L, 6L))
 
   expect_equal(hc[c("labels", "order")],bl)
