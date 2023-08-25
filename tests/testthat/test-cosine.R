8ba4c1b02dddc23785bd5e09a025e63e1e919864
Gregory Jefferis <jefferis@gmail.com>
2023-08-25 12:03:49 +0100

    improved handling of user-supplied labRow argument
    * make sure they line up with neuron order (or error if this can't be guaranteed)
    * with tests

--- tests/testthat/test-cosine.R
+++ tests/testthat/test-cosine.R
@@ -3,7 +3,16 @@ test_that("multiplication works", {
     cf_cosine_plot(cf_ids(hemibrain="/type:LAL00.+"), group = F, heatmap = F),
     'hclust')
 
-  expect_s3_class(
-    cf_cosine_plot(cf_ids(hemibrain="/type:LAL00.+"), group = T, heatmap = F),
-    'hclust')
+  lalmeta=cf_meta(cf_ids(hemibrain="/type:LAL00.+"))
+  # NB left_join requires the id columns to have the same character data type
+  mytypes=data.frame(
+    id=as.character(c(5813047453, 1011611587)),
+    mytype=c("alice", 'bob'))
+  lalmeta2=dplyr::left_join(lalmeta, mytypes, by='id') %>%
+    mutate(label=glue::glue('{type}_{side} :: {mytype}'))
+
+  expect_type(
+    dend <- lalmeta2 %>% with(cf_cosine_plot(key, labRow=label, heatmap = T)),
+    'list')
+  expect_error(cf_cosine_plot(cf_ids(hemibrain="/type:LAL00.+"), letters[1:15]))
 })
