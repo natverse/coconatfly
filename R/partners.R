d3cad5ae5a0d217ff607b18da91e62026382f3af
Gregory Jefferis <jefferis@gmail.com>
2023-12-04 23:46:55 +0000

    fix handling of manc partners
    * this had been missing for a long time it seems

--- R/partners.R
+++ R/partners.R
@@ -76,6 +76,9 @@ cf_partners <- function(ids, threshold=1L, partners=c("inputs", "outputs"),
         ))
     } else if (n=='fanc') {
       tres=fancr::fanc_partner_summary(ids[[n]], partners = partners, threshold = threshold)
+    } else if(n=='manc') {
+      tres=malevnc::manc_connection_table(ids[[n]],partners = partners, threshold=threshold, chunk = neuprint.chunksize)
+      tres %>% dplyr::select(partner, type, name) %>% dplyr::rename(bodyid=partner)
     }
     tres=coconat:::standardise_partner_summary(tres)
     tres$dataset=n
