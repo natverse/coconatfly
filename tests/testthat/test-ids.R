6d02b220fbf73ecb2cd8a9dc677915bd698d8466
Gregory Jefferis <jefferis@gmail.com>
2024-08-24 12:42:16 +0100

    test print.cidlist()

--- tests/testthat/test-ids.R
+++ tests/testthat/test-ids.R
@@ -30,11 +30,13 @@ test_that("key handling works", {
 
   expect_warning(cf_ids(hemibrain = 'rhubarb', expand = T))
 
-  expect_equal(c(
+  expect_equal(res <- c(
     cf_ids(hemibrain = '/MBON0[12].*', flywire=1:3),
     cf_ids(hemibrain = '612371421', flywire=3:5)),
     cf_ids(hemibrain = c("612371421", "673509195", "424789697", "5813022341"),
          flywire=as.character(1:5)))
+
+  expect_output(print(res), regexp = 'flywire.*hemibrain')
 })
 
 test_that("fanc/banc ids/metadata", {
