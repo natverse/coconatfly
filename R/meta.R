2edde252fd9ee413cb4c2b8e362ac18bcfdbef3d
Gregory Jefferis <jefferis@gmail.com>
2024-08-31 02:09:20 +0100

    Use optional dataframe to supplement fanc_meta
    * this is a hack of course
    * if we keep it then we should try to make it more generic
      and allow other datasets to do something similar

--- R/meta.R
+++ R/meta.R
@@ -172,7 +172,28 @@ manc_meta <- function(ids, ...) {
 
 fanc_meta <- function(ids=NULL, ...) {
   ids=fanc_ids(ids)
-  fancr::with_fanc(fancorbanc_meta(table='neuron_information', ids=ids, ...))
+  df=fancr::with_fanc(fancorbanc_meta(table='neuron_information', ids=ids, ...))
+  metaf=getOption('coconatfly.fancmeta')
+  if(!is.null(metaf)) {
+    ext=tools::file_ext(metaf)
+    df2=if(ext=='tsv') {
+      data.table::fread(metaf, integer64 = 'character')
+    } else if(ext=='feather') {
+      arrow::read_feather(metaf)
+    } else stop("Unsupported extension:", ext, " for FANC metadata file!")
+
+    df2$root_id=fancr::with_fanc(fafbseg::flywire_updateids(df2$root_id, df2$supervoxel_id, version = fanc_version()))
+    df2 <- df2 |>
+      rename(id=root_id) |>
+      select(-supervoxel_id, -cell_id)
+    df=dplyr::bind_rows(df2, df)
+    df <- df |>
+      filter(!duplicated(id))
+    if(length(ids)>0) {
+      df <- left_join(data.frame(id=ids), df, by='id')
+    }
+  }
+  df
 }
 
 banc_meta <- function(ids=NULL, ...) {
